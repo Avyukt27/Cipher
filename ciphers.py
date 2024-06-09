afa82736c2cf36e14ee943b6c093144a57d5a908
Avyukt27 <avyukt.aggarwal007@gmail.com>
2024-06-09 18:00:38 +0530

    Edited Caeser Cipher

--- ciphers.py
+++ ciphers.py
@@ -1,11 +1,40 @@
-def caeser_cipher(plaintext: str, shift: int, decrypt: bool) -> str:
-    text_lst: list[str] = list(plaintext)
+from itertools import islice, cycle
+
+def get_letter_value(letter: str) -> int:
+    return ord(letter) - 64
+
+def get_letter_from_value(value: int) -> str:
+    return chr(value + 64)
+
+def caeser_cipher(text: str, shift: int, decrypt: bool) -> str:
+    text_lst: list[str] = list(text)
     result: str = ""
 
+    if decrypt:
+        shift = -shift
+
     for letter in text_lst:
-        value: int = ord(letter)
+        value: int = get_letter_value(letter)
         if not decrypt:
-            result += chr(value + shift)
-        else:
-            result += chr(value - shift)
+            result += get_letter_from_value(value + shift)
+            
     return result
+
+def vigenère_cipher(text: str, key: str, decrypt: bool) -> str:
+    text_lst: list[str] = list(text)
+    key_lst: list[str] = list(key)
+
+    if len(key_lst) < len(text_lst):
+        key_lst = list(islice(cycle(key_lst), len(text_lst)))
+    if len(key_lst) > len(text_lst):
+        key_lst = key_lst[:len(key_lst)]
+
+    result: str = ""
+
+    for index, letter in enumerate(text_lst):
+        letter_value: int = get_letter_value(letter)
+        key_value: int = get_letter_value(key_lst[index])
+        new_value: int = letter_value + key_value
+
+    return result
+print(vigenère_cipher("DCODE", "KEY", False))

8e8cbe6753b70bdc4c7521c065a6194b4da28924
Avyukt27 <avyukt.aggarwal007@gmail.com>
2024-06-09 21:53:40 +0530

    Added Rail Fence Cipher

--- ciphers.py
+++ ciphers.py
@@ -61,9 +61,67 @@ def vigenère_cipher(text: str, key: str, decode: bool = False) -> str:
 
     return result
 
+def rail_fence_cipher(text: str, rails: int, decode: bool = False):
+    '''
+    Rail Fence Cipher\n
+    Uses a Rail Fence (Zig-Zag) Cipher on {text} with {rails} rails\n
+    Set {decode} to True to decode {text} with {rails} rails
+    '''
+    # Make everything Upper Case
+    text.upper()
+    
+    # Make Rail Fence
+    rail_fence = [[""]*len(text) for _ in range(rails)]
+
+    # Variables to move the cursor
+    direction = -1
+    row = 0
+
+    if decode:  # Decoding
+        # Fill the rail_fence with placeholders
+        for col in range(len(text)):
+            rail_fence[row][col] = '*'
+
+            # Change direction if we've hit the top or bottom rail
+            if row == 0 or row == rails - 1:
+                direction *= -1
+
+            # Move to the next row
+            row += direction
+
+        # Fill the rail rail_fence with the ciphertext
+        i = 0
+        for row in range(rails):
+            for col in range(len(text)):
+                if rail_fence[row][col] == '*':
+                    rail_fence[row][col] = text[i]
+                    i += 1
+
+        # Extract the plaintext from the rail_fence
+        result = [rail_fence[row][col] for col in range(len(text)) for row in range(rails) if rail_fence[row][col] is not None]
+
+    else:  # Encoding
+        # Fill the rail rail_fence
+        for col in range(len(text)):
+            rail_fence[row][col] = text[col]
+
+            # Change direction if we've hit the top or bottom rail
+            if row == 0 or row == rails - 1:
+                direction *= -1
+
+            # Move to the next row
+            row += direction
+
+        # Extract the text from the rail_fence
+        result = [rail_fence[row][col] for row in range(rails) for col in range(len(text)) if rail_fence[row][col] is not None]
+
+    return "".join(result)
+
 # Testing
 if __name__ == '__main__':
     print(f"Caeser Cipher (Shift: 3→)\nEncoding: {caeser_cipher("HELLO", 3)}")
     print(f"Decoding: {caeser_cipher("KHOOR", 3, True)}\n")
     print(f"Vigenère Cipher (Key: 'KEY')\nEncoding: {vigenère_cipher('HELLO', 'KEY')}")
-    print(f"Decodeing: {vigenère_cipher('RIJVS', 'KEY', True)}\n")
+    print(f"Decoding: {vigenère_cipher('RIJVS', 'KEY', True)}\n")
+    print(f"Rail Fence Cipher (Key: 'KEY')\nEncoding: {rail_fence_cipher('HELLO', 3)}")
+    print(f"Decoding: {rail_fence_cipher('HOELL', 3, True)}\n")

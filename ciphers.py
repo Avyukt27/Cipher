c6d0f875235ab6918827e4bc23ad1d03079a4177
Avyukt27 <avyukt.aggarwal007@gmail.com>
2024-06-09 16:51:15 +0530

    Added Caeser Cipher

--- ciphers.py
+++ ciphers.py
@@ -1,1 +1,11 @@
-def caeser_cipher(plaintext: str, shift: int, decrypt: bool):
+def caeser_cipher(plaintext: str, shift: int, decrypt: bool) -> str:
+    text_lst: list[str] = list(plaintext)
+    result: str = ""
+
+    for letter in text_lst:
+        value: int = ord(letter)
+        if not decrypt:
+            result += chr(value + shift)
+        else:
+            result += chr(value - shift)
+    return result

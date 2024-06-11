bd5c9cd1d08ba125822afff3b8e7032bdc679a40
Avyukt27 <avyukt.aggarwal007@gmail.com>
2024-06-11 20:11:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup, find_packages
 
 VERSION = '1.0.1'
 DESCRIPTION = 'Encrypt and Decrypt text'
-LONG_DESCRIPTION = 'A package that allows encryption and decryption of text using methods.'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 # Setting up
 setup(
@@ -11,7 +13,7 @@ setup(
     author="Avyukt27",
     author_email="<avyukt.aggarwal007@gmail.com>",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[],
     keywords=['python', 'cipher', 'encode', 'encrypt', 'decrypt', 'decode'],

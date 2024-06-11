69280e46a2904a9f2056a94bc8551fc028f262b6
Avyukt27 <avyukt.aggarwal007@gmail.com>
2024-06-11 19:39:10 +0530

    Updates to fils

--- setup.py
+++ setup.py
@@ -11,17 +11,8 @@ setup(
     author="Avyukt27",
     author_email="<avyukt.aggarwal007@gmail.com>",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=[],
     keywords=['python', 'cipher', 'encode', 'encrypt', 'decrypt', 'decode'],
-    classifiers=[
-        "Development Status :: 3 - First Release",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
 )

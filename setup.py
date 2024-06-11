e71a21faf064296836061e51bf41b4c4945c3ae8
Avyukt27 <avyukt.aggarwal007@gmail.com>
2024-06-11 19:53:34 +0530

    First Build

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Encrypt and Decrypt text'
 LONG_DESCRIPTION = 'A package that allows encryption and decryption of text using methods.'
 
 # Setting up
 setup(
-    name="ciphers",
+    name="ciphers-module",
     version=VERSION,
     author="Avyukt27",
     author_email="<avyukt.aggarwal007@gmail.com>",
